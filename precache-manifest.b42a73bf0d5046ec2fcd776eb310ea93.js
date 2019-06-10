7f963e8c6b9b9fb9e3c35e6b4ef1ccdc55aec108
Vadim Veksler <reyzele91@gmail.com>
2019-06-10 15:08:11 +0300

    .

---
+++ precache-manifest.b42a73bf0d5046ec2fcd776eb310ea93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "90de0040f92dc05cf5459e454287cd8f",
+    "url": "./static/media/sechin.90de0040.png"
+  },
+  {
+    "revision": "056c76540183e206e002a1c81882539d",
+    "url": "./static/media/bg.056c7654.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "283a9bb44ebec859c53b",
+    "url": "./static/js/main.283a9bb4.chunk.js"
+  },
+  {
+    "revision": "3a58f11f13f5574bd1fa",
+    "url": "./static/js/1.3a58f11f.chunk.js"
+  },
+  {
+    "revision": "283a9bb44ebec859c53b",
+    "url": "./static/css/main.ccc6cde5.chunk.css"
+  },
+  {
+    "revision": "fff4ba69f04c06c2334df370b4f48400",
+    "url": "./index.html"
+  }
+];
